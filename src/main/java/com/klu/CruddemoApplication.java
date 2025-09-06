48b977c9d0ec70ca163658fc837eab8fa8b22303
2300030948 <162714171+2300030948@users.noreply.github.com>
2025-09-06 14:45:02 +0530

    Update CruddemoApplication.java

--- src/main/java/com/klu/CruddemoApplication.java
+++ src/main/java/com/klu/CruddemoApplication.java
@@ -2,12 +2,18 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class CruddemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CruddemoApplication.class);
+    }
 }
